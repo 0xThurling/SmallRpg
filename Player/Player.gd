40850aaf2e4200782cf3ea1ee120743f22b76b3a
Gl4SS3S <jthurling78@gmail.com>
2024-04-10 18:39:07 +0200

    Added tilesets to the game and added collisions to those tilesets

--- Player/Player.gd
+++ Player/Player.gd
@@ -4,15 +4,34 @@ const MAX_SPEED = 120
 const ACCELERATION = 3500
 const FRICTION = 1500
 
+enum {
+	MOVE,
+	ROLL,
+	ATTACK
+}
+
+var state = MOVE
+
 @onready var animation_player = $AnimationPlayer
 @onready var animation_tree = $AnimationTree
 @onready var animation_state = animation_tree.get("parameters/playback")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	animation_tree.active = true
 	pass
 
 func _physics_process(delta):
+	print_debug(state)
+	match state:
+		MOVE:
+			move_state(delta)
+		ROLL:
+			pass
+		ATTACK:
+			attack_state(delta)
+
+func move_state(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
@@ -21,6 +40,7 @@ func _physics_process(delta):
 	if input_vector != Vector2.ZERO:
 		animation_tree.set("parameters/Idle/blend_position", input_vector)
 		animation_tree.set("parameters/Run/blend_position", input_vector)
+		animation_tree.set("parameters/Attack/blend_position", input_vector)
 		animation_state.travel("Run")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
@@ -28,3 +48,13 @@ func _physics_process(delta):
 		velocity = velocity.move_toward(Vector2.ZERO, delta * FRICTION)
 	
 	move_and_slide()
+	
+	if Input.is_action_just_pressed("attack"):
+		state = ATTACK
+	
+func attack_state(delta):
+	animation_state.travel("Attack")
+	
+func attack_animation_finished():
+	state = MOVE
+	
